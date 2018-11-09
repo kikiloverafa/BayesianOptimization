d9c4551bfd493ad33c9e9872601282bf0ac1aeeb
fmfn <fernando@chatkit.com>
2018-11-09 09:20:39 -0500

    add global datetime to logger

--- bayes_opt/observer.py
+++ bayes_opt/observer.py
@@ -7,8 +7,9 @@ from datetime import datetime
 from .event import Events
 
 
-class _Clock:
+class _Tracker:
     def __init__(self):
+        self._iterations = 0
         self._start_time = None
         self._previous_time = None
 
@@ -23,10 +24,14 @@ class _Clock:
         time_delta = now - self._previous_time
 
         self._previous_time = now
-        return time_elapsed.total_seconds(), time_delta.total_seconds()
+        return (
+            now.strftime("%Y-%m-%d %H:%M:%S"),
+            time_elapsed.total_seconds(),
+            time_delta.total_seconds()
+        )
 
 
-class ScreenLogger(_Clock):
+class ScreenLogger(_Tracker):
     def __init__(self, verbose=0):
         self._verbose = verbose
         super(ScreenLogger, self).__init__()
@@ -50,22 +55,22 @@ class ScreenLogger(_Clock):
                   instance.max)
 
 
-class JSONLogger(_Clock):
+class JSONLogger(_Tracker):
     def __init__(self, path):
         self._path = path if path[-5:] == ".json" else path + ".json"
         try:
             os.remove(self._path)
         except OSError:
             pass
-
         super(JSONLogger, self).__init__()
 
     def update(self, event, instance):
         if event == Events.OPTMIZATION_STEP:
             data = dict(instance.res[-1])
 
-            time_elapsed, time_delta = self._time_metrics()
-            data["time"] = {
+            now, time_elapsed, time_delta = self._time_metrics()
+            data["datetime"] = {
+                "datetime": now,
                 "elapsed": time_elapsed,
                 "delta": time_delta,
             }
